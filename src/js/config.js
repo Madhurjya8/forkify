e56497f431a724ad23cac96aa88b42d54e29ca99
Madhurjya8 <madhurjya.saha@gmail.com>
2022-05-17 17:57:32 +0530

    Added config file to gitignore

--- src/js/config.js
+++
@@ -1,6 +0,0 @@
-export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
-export const TIMEOUT_SEC = 10;
-export const RES_PER_PAGE = 10;
-export const KEY = '0b84e451-d167-43fc-8f15-f07e3a2a7ade';
-export const MODAL_CLOSE_SEC = 2.5;
-// https://forkify-api.herokuapp.com/v2
